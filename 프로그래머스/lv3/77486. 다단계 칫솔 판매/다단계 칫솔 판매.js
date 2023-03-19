679978461b9e5d35d5e82f194104cf2c91a2e5ae
jableee <96569903+jableee@users.noreply.github.com>
2023-03-20 00:15:00 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 12.97 ms, Memory: 49.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -0,0 +1,18 @@
+function solution(enroll, referral, seller, amount) {
+    const members = new Map()
+    enroll.forEach((member, i) => {
+        members.set(member, { referral: referral[i], profit: 0 })
+    })
+    seller.forEach((member, i) => {
+        let curAmount = amount[i] * 100
+        let curMember = members.get(member)
+        while(curAmount && curMember) {
+            let div = Math.floor(curAmount / 10)
+            curMember.profit += curAmount - div
+            curAmount = div
+            curMember = members.get(curMember.referral)
+        }
+    })
+    
+    return enroll.map(member => members.get(member).profit)
+}
